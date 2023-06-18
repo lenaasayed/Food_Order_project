2510e658c8baaecbcb7e186ce20c05cc94b682ba
lenaasayed <lenaa.sayed7@gmail.com>
2023-06-18 17:13:31 +0200

    delete_some_files

--- frontend/angularstack/src/app/app-routing.module.ts
+++ frontend/angularstack/src/app/app-routing.module.ts
@@ -1,36 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { NotfoundComponent } from './notfound/notfound.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { AddProviderComponent } from './provider/add-provider/add-provider.component';
-import { EditProviderComponent } from './provider/edit-provider/edit-provider.component';
-import { DeleteProviderComponent } from './provider/delete-provider/delete-provider.component';
-import { DetailsProviderComponent } from './provider/details-provider/details-provider.component';
-import { ProviderComponent } from './provider/provider.component';
-import { RegisterComponent } from './register/register.component';
-
 let n:number=1;
 const routes: Routes = [
-  {path:"",component:HomeComponent},
-  {path:"about",component:AboutComponent},
-  {path:"contact",component:ContactComponent},
-  {path:"login",component:LoginComponent},
-  {path:"register",component:RegisterComponent},
-  {path:"provider",component:ProviderComponent},
-  {path:"provider/add-provider",component:AddProviderComponent},
-  {path:"provider/edit-provider/:id",component:EditProviderComponent},
-  {path:"provider/delete-provider/:id",component:DeleteProviderComponent},
-  {path:"provider/details-provider/:id",component:DetailsProviderComponent},
+  // {path:"",component:HomeComponent},
+  // {path:"about",component:AboutComponent},
+  // {path:"contact",component:ContactComponent},
+  // {path:"login",component:LoginComponent},
+  // {path:"register",component:RegisterComponent},
+  // {path:"provider",component:ProviderComponent},
+  // {path:"provider/add-provider",component:AddProviderComponent},
+  // {path:"provider/edit-provider/:id",component:EditProviderComponent},
+  // {path:"provider/delete-provider/:id",component:DeleteProviderComponent},
+  // {path:"provider/details-provider/:id",component:DetailsProviderComponent},
 
 
 
-  {path:"protfolio",component:PortfolioComponent},
+  // {path:"protfolio",component:PortfolioComponent},
 
-  {path:"**",component:NotfoundComponent},
+  // {path:"**",component:NotfoundComponent},
 
 ];
 
--- frontend/angularstack/src/app/app.module.ts
+++ frontend/angularstack/src/app/app.module.ts
@@ -6,26 +6,10 @@ import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 // import { AboutComponent } from './about/about.component';
-import { AboutModule } from './about/about.module';
-import { UserModule } from './user/user.module';
-import { HomeComponent } from './home/home.component';
-import { NotfoundComponent } from './notfound/notfound.component';
-import { ContactComponent } from './contact/contact.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { NavComponent } from './nav/nav.component';
-import { FooterComponent } from './footer/footer.component';
-import { ProviderModule } from './provider/provider.module';
 import { HeaderComponent } from './components/partials/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    NotfoundComponent,
-    ContactComponent,
-    PortfolioComponent,
-    NavComponent,
-    FooterComponent,
     HeaderComponent,
 
   ],
@@ -33,9 +17,6 @@ import { HeaderComponent } from './components/partials/header/header.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    AboutModule,
-    UserModule,
-    ProviderModule,
     HttpClientModule
   ],
   providers: [],
